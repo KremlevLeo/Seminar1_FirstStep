32372741ad7a222a88b667213c69371cc2cd1038
Kremlev Leonid <leeonidix@gmail.com>
2024-09-16 13:12:51 +0500

    Third Task Done

---
+++ Tesk3/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Tesk3
+{
+    // Программа от -N до N
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write($"{number} -> ");
+            for (int i = number * (-1); i <= number; i++)
+            {
+                if (i == number) Console.WriteLine(i+";");
+                else Console.Write($"{i}, ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
