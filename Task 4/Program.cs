27af714b95e8cc7a0376bc14c7b3bfd9ecd14e48
Kremlev Leonid <leeonidix@gmail.com>
2024-09-16 13:19:10 +0500

    Fouth Task Done

---
+++ Task 4/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Task_4
+{
+    // Последняя цифра техзначного
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a three-digit number: ");
+            int number= int.Parse(Console.ReadLine());
+            Console.WriteLine($"{number} -> {number%10}");
+            Console.ReadLine();
+        }
+    }
+}
